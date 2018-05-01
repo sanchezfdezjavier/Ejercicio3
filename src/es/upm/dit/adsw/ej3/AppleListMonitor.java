8f89886849afdc85920ebe14298242aa8e4b082c
sanchezfdez.javier@gmail.com <Rogerfederer1>
2018-05-01 20:08:23 +0200

    Declarated AppleListMonitor methods.

--- src/es/upm/dit/adsw/ej3/AppleListMonitor.java
+++ src/es/upm/dit/adsw/ej3/AppleListMonitor.java
@@ -1,5 +1,34 @@
 package es.upm.dit.adsw.ej3;
 
 public class AppleListMonitor {
-    //Escribirla entera
+
+    //Constructor
+    public AppleListMonitor(){
+
+    }
+
+    // Tenemos una manzana mas.
+    public void add(Apple apple){
+
+    }
+
+    // Tenemos una manzana menos.
+    public void remove(Apple apple){
+
+    }
+
+    // Informa si hay alguna manzana cerca del segmento p1-p2.
+    public Apple getCloseApple(XY P1, XY P2){
+        Apple manzanaResultado= null;
+
+        return manzanaResultado;
+
+    }
+
+    //Actua sobre una menzana dentro del segmento p1-p2.
+    public Apple hitCloseApple (XY P1, XY P2){
+        Apple manzanaResultado= null;
+
+        return manzanaResultado;
+    }
 }
