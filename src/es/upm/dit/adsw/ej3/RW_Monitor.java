bc79323338bc22cb47994e82fbeaf125967edfe5
sanchezfdez.javier@gmail.com <Rogerfederer1>
2018-05-02 01:56:27 +0200

    Created RW_Monitor template.

--- src/es/upm/dit/adsw/ej3/RW_Monitor.java
+++ src/es/upm/dit/adsw/ej3/RW_Monitor.java
@@ -1,5 +1,53 @@
 package es.upm.dit.adsw.ej3;
 
+import java.lang.*;
+
 public class RW_Monitor {
-    //Hay que escribirla entera.
+
+    // Constructor
+    public RW_Monitor(){
+
+    }
+
+    // Getter
+    public int getNReadersIn(){
+        int readersNum= 0;
+
+
+        return readersNum;
+    }
+
+    // Getter
+    public int getNWritersIn(){
+        int writersNum=0;
+
+        return writersNum;
+    }
+
+    // Solicitud de permiso para hacer una lectura. El thread que llama se queda esperando hasta que pueda entrar.
+    public void openReading(){
+
+    }
+
+    // Devuelve el permiso de lectura.
+    public void closeReading() throws IllegalMonitorStateException{
+        try{
+            //Codigo
+        }catch(IllegalMonitorStateException e){
+           System.out.println("Error en  el metodo 'closeWriting' de la clase 'RW_Monitor'");
+        }
+    }
+
+    // Solicitud de permiso para hacer una escritura. El thread que llama se queda esperando hasta que pueda entrar.
+    public void openWriting(){
+
+    }
+
+    public void closeWriting() throws IllegalMonitorStateException{
+        try{
+            //Codigo
+        }catch(IllegalMonitorStateException e){
+            System.out.println("Error en el método 'closeWriting' de la clase 'RW_Monitor");
+        }
+    }
 }
